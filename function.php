432b5ce3745443d94e970c93c768b71395eb9614
Khalid Almallahi <abukotsh@gmail.com>
2020-04-01 09:26:32 +0300

    Create function.php

---
+++ function.php
@@ -0,0 +1,33 @@
+/**
+ * @snippet       Dispatch Date @ WooCommerce Single Product
+ * @author        Khalid Almallahi
+ * @testedwith    WooCommerce 3.9
+ */
+ 
+add_action( 'woocommerce_after_add_to_cart_form', 'abukotsh_dispatch_info_single_product' );
+    
+function abukotsh_dispatch_info_single_product() {
+   date_default_timezone_set( 'Europe/London' );  
+    
+   // if FRI/SAT/SUN delivery will be MON
+   if ( date( 'N' ) >= 5 ) {
+      $del_day = date( "l jS F", strtotime( "next monday" ) );
+      $order_by = "Monday";
+   } 
+    
+   // if MON/THU after 4PM delivery will be TOMORROW
+   elseif ( date( 'H' ) >= 16 ) {
+      $del_day = date( "l jS F", strtotime( "tomorrow" ) );
+      $order_by = "tomorrow";
+   } 
+    
+   // if MON/THU before 4PM delivery will be TODAY
+   else {
+      $del_day = date( "l jS F", strtotime( "today" ) );
+      $order_by = "today";
+   }
+ 
+   $html = "<br><div class='woocommerce-message' style='clear:both'>Order by 4PM {$order_by} for delivery on {$del_day}</div>";
+    
+   echo $html;
+}
